ed7565de9453bc774c455dfa532c6fe7a73f5927
ferhen <Fernando_Eckstein@hotmail.com>
2022-11-04 14:54:02 -0300

    Add project files.

---
+++ XMLValidator.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace XMLValidator.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ XMLValidator/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace XMLValidator
+{
+    public class Class1
+    {
+
+    }
+}
